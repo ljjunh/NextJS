5c4a9b716ff25600f2676866abbb4f6fcb2a95e0
junhee <limjunhee4575@gmail.com>
2024-08-23 00:35:49 +0900

    24/08/23

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -1,11 +1,21 @@
+import GlobalLayout from "@/components/global-layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <div>글로벌 헤더</div>
-      <Component {...pageProps} />
-    </>
-  );
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+export default function App({
+  Component,
+  pageProps,
+}: AppProps & {
+  Component: NextPageWithLayout;
+}) {
+  // 컴포넌트에 getLayout 메서드가 없으면 컴포넌트 그대로 리턴
+  const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  return <GlobalLayout>{getLayout(<Component {...pageProps} />)}</GlobalLayout>;
 }
