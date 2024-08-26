4406abe359f6c9088d1b7c72660e5ba51ffa2519
junhee <limjunhee4575@gmail.com>
2024-08-27 00:59:19 +0900

    24/08/26

---
+++ section05/src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,41 @@
+import BookItem from "@/components/book-item";
+import BookListSkeleton from "@/components/skeleton/book-list-skeletion";
+import { BookData } from "@/types";
+import { delay } from "@/utill/delay";
+import { Suspense } from "react";
+
+async function SearchResult({ q }: { q: string }) {
+  await delay(1500);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
+    { cache: "force-cache" }
+  );
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const books: BookData[] = await response.json();
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense
+      key={searchParams.q || ""}
+      fallback={<BookListSkeleton count={3} />}
+    >
+      <SearchResult q={searchParams.q || ""} />;
+    </Suspense>
+  );
+}
